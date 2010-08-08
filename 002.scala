d6e746d7f8d186dbbcbf08ef9a493db62d3225b0
Shoichiro OISHI <coffee.potato@gmail.com>
2010-08-08 15:22:54 +0900

    3

--- 002.scala
+++ 002.scala
@@ -1,7 +1,21 @@
 import scala.io.Source
 
+def maxIn(ls: List[Int]) = 
+  ls.reduceLeft((x, y) => x max y)
+
+def lengthOfLength(ls: List[String]) =
+  ls.map(l => l.length.toString.length)
+
+def printLengthesAndLines(ls: List[String]) = {
+  val maxLengthOfLength = maxIn(lengthOfLength(ls))
+  for (l <- ls) {
+    val len = l.length
+    val padding = " " * (maxLengthOfLength - len)
+    print(len + padding + " |" + l)
+  }
+}
+
 if (args.length == 1) {
-  val lines = Source.fromFile(args(0)).getLines
-  lines.foreach(l => print(l.length + " " + l))
+  printLengthesAndLines(Source.fromFile(args(0)).getLines.toList)
 }
   
